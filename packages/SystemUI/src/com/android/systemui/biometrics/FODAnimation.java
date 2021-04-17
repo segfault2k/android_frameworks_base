4b919b378df79745f5558324123dc464820edb98
Ritzz <riteshm321@gmail.com>
2021-04-17 16:44:32 +0200

    Revert "FODAnimation: Make sure we don't add the view twice"
    This reverts commit 5cb4eea0a8cf9df0de722c7c9a9210fae799cc97.

--- packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
@@ -40,7 +40,6 @@ public class FODAnimation extends ImageView {
     private WindowManager mWindowManager;
     private boolean mIsKeyguard;
     private boolean mIsRecognizingAnimEnabled;
-    private boolean mIsAdded;
 
     private int mSelectedAnim;
     private final int[] ANIMATION_STYLES = {
@@ -119,14 +118,11 @@ public class FODAnimation extends ImageView {
     public void showFODanimation() {
         if (mAnimParams != null && !mShowing && mIsKeyguard) {
             mShowing = true;
-            if (!mIsAdded) {
-                mIsAdded = true;
-                if (this.getWindowToken() == null) {
-                    mWindowManager.addView(this, mAnimParams);
-                    mWindowManager.updateViewLayout(this, mAnimParams);
-                }
-                recognizingAnim.start();
+            if (this.getWindowToken() == null){
+                mWindowManager.addView(this, mAnimParams);
+                mWindowManager.updateViewLayout(this, mAnimParams);
             }
+            recognizingAnim.start();
         }
     }
 
@@ -140,7 +136,6 @@ public class FODAnimation extends ImageView {
             }
             if (this.getWindowToken() != null) {
                 mWindowManager.removeView(this);
-                mIsAdded = false;
             }
         }
     }
