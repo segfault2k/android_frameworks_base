bf7f09f2d5b838405399db86cb652563f636c23f
Ritzz <riteshm321@gmail.com>
2021-04-17 17:07:21 +0200

    Revert "FODAnimation: Allow to set vertical position offset"
    This reverts commit eb965e05d2103b6f6bfd5641db46c6d2fa6bc904.

--- packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
@@ -35,7 +35,7 @@ public class FODAnimation extends ImageView {
     private boolean mShowing = false;
     private Context mContext;
     private int mAnimationSize;
-    private int mAnimationOffset;
+    private int mAnimationPositionY;
     private AnimationDrawable recognizingAnim;
     private WindowManager mWindowManager;
     private boolean mIsKeyguard;
@@ -74,7 +74,6 @@ public class FODAnimation extends ImageView {
         mWindowManager = mContext.getSystemService(WindowManager.class);
 
         mAnimationSize = mContext.getResources().getDimensionPixelSize(R.dimen.fod_animation_size);
-        mAnimationOffset = mContext.getResources().getDimensionPixelSize(R.dimen.fod_animation_offset);
         mAnimParams.height = mAnimationSize;
         mAnimParams.width = mAnimationSize;
 
@@ -83,7 +82,7 @@ public class FODAnimation extends ImageView {
         mAnimParams.flags =  WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
         mAnimParams.gravity = Gravity.TOP | Gravity.CENTER;
-        mAnimParams.y = mPositionY - (mAnimationSize / 2) + mAnimationOffset;
+        mAnimParams.y = mPositionY - (mAnimationSize / 2);
 
         setScaleType(ImageView.ScaleType.CENTER_INSIDE);
         update();
@@ -98,7 +97,7 @@ public class FODAnimation extends ImageView {
     }
 
     public void updateParams(int mDreamingOffsetY) {
-        mAnimParams.y = mDreamingOffsetY - (mAnimationSize / 2) + mAnimationOffset;
+        mAnimParams.y = mDreamingOffsetY - (mAnimationSize / 2);
     }
 
     public void setAnimationKeyguard(boolean state) {
